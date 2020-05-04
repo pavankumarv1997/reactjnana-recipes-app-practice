ac96965328d6ab491a7c39cad12be5d7fa0ff237
pavankumarv1997 <pavankumarv287.pk@gmail.com>
2020-05-05 01:19:37 +0530

    more items demo

--- src/data/tempList.js
+++ src/data/tempList.js
@@ -26,4 +26,31 @@ export const recipeData = [
     image_url:
       "https://tastykitchen.com/recipes/wp-content/uploads/sites/2/2009/10/Crash-Hot-Potatoes-420x278.png",
   },
+  {
+    publisher: "Closet Cooking",
+    title: "Jalapeno Popper Grilled Cheese Sandwich",
+    source_url:
+      "http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html",
+    recipe_id: "35382",
+    image_url:
+      "https://www.closetcooking.com/wp-content/uploads/2013/04/JalapenoPopperGrilledCheeseSandwich1500.jpg",
+  },
+  {
+    publisher: "The Pioneer Woman",
+    title: "Perfect Iced Coffee",
+    source_url:
+      "http://thepioneerwoman.com/cooking/2011/06/perfect-iced-coffee/",
+    recipe_id: "47024",
+    image_url:
+      "https://thepioneerwoman.com/wp-content/uploads/2013/06/iced3.jpg?resize=426%2C640",
+  },
+  {
+    publisher: "The Pioneer Woman",
+    title: "Crash Hot Potatoes",
+    source_url:
+      "http://thepioneerwoman.com/cooking/2008/06/crash-hot-potatoes/",
+    recipe_id: "47319",
+    image_url:
+      "https://tastykitchen.com/recipes/wp-content/uploads/sites/2/2009/10/Crash-Hot-Potatoes-420x278.png",
+  },
 ];
