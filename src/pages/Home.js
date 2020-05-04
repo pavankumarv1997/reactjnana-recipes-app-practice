7bfeaf6ce1fdb2f96b8e790545a6ca659fe3a47b
pavankumarv1997 <pavankumarv287.pk@gmail.com>
2020-05-05 01:17:24 +0530

    title change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default class Home extends Component {
   render() {
     return (
-      <Header>
+      <Header title="Amazing Recipes">
         <Link
           to="/recipes"
           className="btn btn-secondary text-uppercase btn-lg mt-3"
